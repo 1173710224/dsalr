7f6bf82edb7c76c8567b20d989eaec64e2ca6240
1173710224 <bozhouchen@hit.edu.cn>
2021-09-23 20:49:44 +0000

    run exp

--- trainers.py
+++ trainers.py
@@ -1,20 +1,22 @@
-from utils import Data
 import torch
 from const import *
 import torch.nn.functional as F
-from models import Mlp, GLMlp, DeepConv
 from optim import Dsa
 import warnings
+from sklearn.metrics import precision_recall_fscore_support as metrics
+import numpy as np
+from models import My_loss
 warnings.filterwarnings("ignore")
 
 
 class Trainer():
-    def __init__(self, train_data, test_data, model, lr=0.001) -> None:
+    def __init__(self, train_data, test_data, model, lr=0.001, opt="adam") -> None:
         self.x = train_data[0]
         self.y = train_data[1]
         self.test_data = test_data
         self.model = model
         self.lr = lr
+        self.opt = opt
         if torch.cuda.is_available():
             self.model.cuda()
             self.device = "cuda"
@@ -23,45 +25,72 @@ class Trainer():
         else:
             self.model.cpu()
             self.device = "cpu"
+        self.acc = []
+        self.loss = []
+        self.p = []
+        self.r = []
+        self.f1 = []
+        self.alpha = []
         pass
 
     def train(self):
-        self.model.train()
-        # optimizer = self.get_opt(ADAM)
-        optimizer = self.get_opt(DSA)
+        optimizer = self.get_opt()
         for i in range(EPOCHSDENSE):
+            self.model.train()
             x, y = self.x, self.y
             preds = self.model(x)
-            # loss = F.mse_loss(preds, y)
             loss = F.cross_entropy(preds, y.long())
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
-            print("Epoch~{}->loss:{}\n".format(i + 1, loss.item()))
-            # for param in self.model.parameters():
-            #     print(param[0][0])
-            #     print(param.grad[0][0])
-            #     break
-        return
+            acc, p, r, f1 = self.val()
+            print("Epoch~{}->loss:{}\nval:{},".format(i +
+                  1, loss.item(), acc), end="")
+            self.loss.append(loss.item())
+            self.p.append(p)
+            self.r.append(r)
+            self.f1.append(f1)
+            self.acc.append(acc)
+        return self.acc, self.p, self.r, self.f1, self.loss
 
-    def get_opt(self, opt="adam"):
-        if opt == ADAM:
+    def get_opt(self):
+        if self.opt == ADAM:
             return torch.optim.Adam(
                 self.model.parameters(), lr=self.lr)
-        if opt == DSA:
+        if self.opt == DSA:
             return Dsa(self.model.parameters())
+        if self.opt == ADAMW:
+            return torch.optim.AdamW(self.model.parameters(), lr=self.lr)
+
+        if self.opt == ADAMAX:
+            return torch.optim.Adamax(self.model.parameters(), lr=self.lr)
+        if self.opt == ADAGRAD:
+            return torch.optim.Adagrad(self.model.parameters(), lr=self.lr)
+        if self.opt == ADADELTA:
+            return torch.optim.Adadelta(self.model.parameters(), lr=self.lr)
+
+        if self.opt == SGD:
+            return torch.optim.SGD(self.model.parameters(), lr=self.lr)
+        if self.opt == RMSPROP:
+            return torch.optim.RMSprop(self.model.parameters(), lr=self.lr)
+        if self.opt == MOMENTUM:
+            return torch.optim.SGD(self.model.parameters(), lr=self.lr, momentum=P_MOMENTUM)
         return None
 
     def val(self):
         x, y = self.test_data
         self.model.eval()
         preds = self.model(x)
+        Preds = preds.detach().cpu().numpy()
+        Preds = [np.argmax(Preds[i]) for i in range(len(preds))]
+        Y = y.detach().cpu().numpy()
+        p, r, f1, _ = metrics(Preds, Y)
         accu = torch.sum(preds.max(1)[1].eq(y).double())/len(y)
-        return accu
+        return accu.cpu().numpy(), p, r, f1
 
 
 class BatchTrainer():
-    def __init__(self, train_loader, test_loader, model, lr=0.001) -> None:
+    def __init__(self, train_loader, test_loader, model, lr=0.001, opt=ADAM) -> None:
         self.train_loader = train_loader
         self.test_loader = test_loader
         self.model = model
@@ -72,11 +101,18 @@ class BatchTrainer():
             self.model.cpu()
             self.device = "cpu"
         self.lr = lr
+        self.opt = opt
+
+        self.acc = []
+        self.loss = []
+        self.p = []
+        self.r = []
+        self.f1 = []
+        self.alpha = []
         pass
 
-    def train(self):
-        self.optimizier = torch.optim.Adam(
-            self.model.parameters(), lr=self.lr)
+    def minibatch_train(self):
+        self.optimizier = self.get_opt()
         self.model.train()
         for i in range(EPOCHS):
             loss_sum = 0
@@ -89,21 +125,53 @@ class BatchTrainer():
                     imgs = imgs.cpu()
                     label = label.cpu()
                 preds = self.model(imgs)
+                # print(preds[0])
                 loss = F.cross_entropy(preds, label)
+                # print(loss.item())
                 self.optimizier.zero_grad()
                 loss.backward()
                 self.optimizier.step()
                 loss_sum += loss.item() * len(imgs)
                 img_num += len(imgs)
-                # print(loss.item())
+                # print(img_num)
             avg_loss = loss_sum * 1.0/img_num
-            print("Epoch~{}->{}".format(i+1, avg_loss))
+            acc, p, r, f1 = self.val()
+            print("Epoch~{}->loss:{}\nval:{},".format(i +
+                  1, avg_loss, acc), end="")
+            self.loss.append(avg_loss)
+            self.p.append(p)
+            self.r.append(r)
+            self.f1.append(f1)
+            self.acc.append(acc)
+        return self.acc, self.p, self.r, self.f1, self.loss
+
+    def batch_train(self):
+        self.optimizier = self.get_opt()
+        for i in range(EPOCHS):
+            self.model.train()
+            self.optimizier.zero_grad()
+            loss_sum = 0
+            for imgs, label in self.train_loader:
+                if torch.cuda.is_available():
+                    imgs = imgs.cuda()
+                    label = label.cuda()
+                else:
+                    imgs = imgs.cpu()
+                    label = label.cpu()
+                preds = self.model(imgs)
+                loss = F.cross_entropy(preds, label)/NUMBATCH
+                loss.backward()
+                loss_sum += loss.item()
+            self.optimizier.step()
+            print("Epoch~{}->{}\nval:{}".format(i+1, loss_sum, self.val()), end=",")
         return
 
     def val(self):
         self.model.eval()
         ncorrect = 0
         nsample = 0
+        Preds = []
+        Y = []
         for imgs, label in self.test_loader:
             if torch.cuda.is_available():
                 imgs = imgs.cuda()
@@ -112,28 +180,210 @@ class BatchTrainer():
                 imgs = imgs.cpu()
                 label = label.cpu()
             preds = self.model(imgs)
+            tmp = preds.detach().cpu().numpy()
+            Preds.extend([np.argmax(tmp[i]) for i in range(len(tmp))])
+            Y.extend(label.detach().cpu().numpy())
             ncorrect += torch.sum(preds.max(1)[1].eq(label).double())
             nsample += len(label)
-        return ncorrect/nsample
+        p, r, f1, _ = metrics(Preds, Y)
+        return (ncorrect/nsample).cpu().numpy(), p, r, f1
+
+    def train(self, lr_init, meta_lr):
+        # self.optimizier = Dsa(
+        #     self.model.parameters(), lr_init, meta_lr)
+        # # self.optimizier = torch.optim.Adam(
+        # #     self.model.parameters(), lr=self.lr)
+        self.optimizier = self.get_opt()
+        self.model.train()
+        for i in range(EPOCHS):
+            loss_sum = 0
+            img_num = 0
+            for imgs, label in self.train_loader:
+                if torch.cuda.is_available():
+                    imgs = imgs.cuda()
+                    label = label.cuda()
+                else:
+                    imgs = imgs.cpu()
+                    label = label.cpu()
+                preds = self.model(imgs)
+                # print(preds[0])
+                loss = F.cross_entropy(preds, label)
+                # print(loss.item())
+                self.optimizier.zero_grad()
+                loss.backward()
+                self.optimizier.step()
+                loss_sum += loss.item() * len(imgs)
+                img_num += len(imgs)
+                # print(img_num)
+            avg_loss = loss_sum * 1.0/img_num
+            # print("Epoch~{}->{}".format(i+1, avg_loss))
+            print("Epoch~{}->{}\nval:{}".format(i+1,
+                  avg_loss, self.val()), end=",")
+        return
+
+    def get_opt(self):
+        if self.opt == ADAM:
+            return torch.optim.Adam(
+                self.model.parameters(), lr=self.lr)
+        if self.opt == DSA:
+            return Dsa(self.model.parameters())
+        if self.opt == ADAMW:
+            return torch.optim.AdamW(self.model.parameters(), lr=self.lr)
+
+        if self.opt == ADAMAX:
+            return torch.optim.Adamax(self.model.parameters(), lr=self.lr)
+        if self.opt == ADAGRAD:
+            return torch.optim.Adagrad(self.model.parameters(), lr=self.lr)
+        if self.opt == ADADELTA:
+            return torch.optim.Adadelta(self.model.parameters(), lr=self.lr)
+
+        if self.opt == SGD:
+            return torch.optim.SGD(self.model.parameters(), lr=self.lr)
+        if self.opt == RMSPROP:
+            return torch.optim.RMSprop(self.model.parameters(), lr=self.lr)
+        if self.opt == MOMENTUM:
+            return torch.optim.SGD(self.model.parameters(), lr=self.lr, momentum=P_MOMENTUM)
+        return None
+
+
+class Case_1_Trainer():
+    def __init__(self, train_data, test_data, model, lr=0.001, opt="adam") -> None:
+        self.x = train_data[0]
+        self.y = train_data[1]
+        self.test_data = test_data
+        self.model = model
+        self.lr = lr
+        self.opt = opt
+        if torch.cuda.is_available():
+            self.model.cuda()
+            self.device = "cuda"
+            self.x = self.x.cuda()
+            self.y = self.y.cuda()
+        else:
+            self.model.cpu()
+            self.device = "cpu"
+        self.loss = []
+        pass
+
+    def train(self):
+        optimizer = self.get_opt()
+        for _ in range(EPOCHSDENSE):
+            self.model.train()
+            x, y = self.x, self.y
+            preds = self.model(x)
+            loss = F.cross_entropy(preds, y.long())
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            self.loss.append(loss.item())
+        return self.loss
+
+    def get_opt(self):
+        if self.opt == ADAM:
+            return torch.optim.Adam(
+                self.model.parameters(), lr=self.lr)
+        if self.opt == DSA:
+            return Dsa(self.model.parameters())
+        if self.opt == ADAMW:
+            return torch.optim.AdamW(self.model.parameters(), lr=self.lr)
+
+        if self.opt == ADAMAX:
+            return torch.optim.Adamax(self.model.parameters(), lr=self.lr)
+        if self.opt == ADAGRAD:
+            return torch.optim.Adagrad(self.model.parameters(), lr=self.lr)
+        if self.opt == ADADELTA:
+            return torch.optim.Adadelta(self.model.parameters(), lr=self.lr)
+
+        if self.opt == SGD:
+            return torch.optim.SGD(self.model.parameters(), lr=self.lr)
+        if self.opt == RMSPROP:
+            return torch.optim.RMSprop(self.model.parameters(), lr=self.lr)
+        if self.opt == MOMENTUM:
+            return torch.optim.SGD(self.model.parameters(), lr=self.lr, momentum=P_MOMENTUM)
+        return None
+
+
+class Casee_2_Trainer():
+    def __init__(self, x, model, lr=0.001, opt="adam") -> None:
+        self.x = x
+        self.model = model
+        self.lr = lr
+        self.opt = opt
+        if torch.cuda.is_available():
+            self.model.cuda()
+            self.device = "cuda"
+            self.x = self.x.cuda()
+        else:
+            self.model.cpu()
+            self.device = "cpu"
+        self.x1 = []
+        self.x2 = []
+        pass
+
+    def train(self):
+        optimizer = self.get_opt()
+        for _ in range(EPOCHSDENSE):
+            self.model.train()
+            x = self.x
+            preds = self.model(x)
+            loss = My_loss(preds)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            self.x = preds
+            Preds = preds.detach().cpu().numpy()
+            self.x1.append(Preds[0])
+            self.x2.append(Preds[1])
+        return self.x1, self.x2
+
+    def get_opt(self):
+        if self.opt == ADAM:
+            return torch.optim.Adam(
+                self.model.parameters(), lr=self.lr)
+        if self.opt == DSA:
+            return Dsa(self.model.parameters())
+        if self.opt == ADAMW:
+            return torch.optim.AdamW(self.model.parameters(), lr=self.lr)
+
+        if self.opt == ADAMAX:
+            return torch.optim.Adamax(self.model.parameters(), lr=self.lr)
+        if self.opt == ADAGRAD:
+            return torch.optim.Adagrad(self.model.parameters(), lr=self.lr)
+        if self.opt == ADADELTA:
+            return torch.optim.Adadelta(self.model.parameters(), lr=self.lr)
+
+        if self.opt == SGD:
+            return torch.optim.SGD(self.model.parameters(), lr=self.lr)
+        if self.opt == RMSPROP:
+            return torch.optim.RMSprop(self.model.parameters(), lr=self.lr)
+        if self.opt == MOMENTUM:
+            return torch.optim.SGD(self.model.parameters(), lr=self.lr, momentum=P_MOMENTUM)
+        return None
 
 
 if __name__ == "__main__":
     # data = Data()
-    # train_data, test_data, ndim, nclass = data.load_car()
+    # # train_data, test_data, ndim, nclass = data.load_car()
     # # train_data, test_data, ndim, nclass = data.load_wine()
+    # # train_data, test_data, ndim, nclass = data.load_iris()
+    # train_data, test_data, ndim, nclass = data.load_agaricus_lepiota()
     # model = Mlp(ndim, nclass)
     # trainer = Trainer(train_data, test_data, model)
     # trainer.train()
     # res = trainer.val()
     # print(res)
 
-    data = Data()
-    train_loader, test_loader, input_channel, ndim, nclass = data.load_mnist()
-    model = DeepConv(input_channel, ndim, nclass)
-    trainer = BatchTrainer(train_loader, test_loader, model)
-    trainer.train()
-    trainer.val()
+    # data = Data()
+    # # train_loader, test_loader, input_channel, ndim, nclass = data.load_cifar10()
+    # train_loader, test_loader, input_channel, ndim, nclass = data.load_mnist()
+    # # model = DeepConv(input_channel, ndim, nclass)
+    # for lr_init in range(-14, -7):
+    #     for meta_lr in [0.00005, 0.0001, 0.0008, 0.001, 0.005, 0.01]:
+    #         st = time()
+    #         print(f"lr_init:{lr_init}, meta_lr:{meta_lr}")
+    #         model = Fmp(input_channel, ndim, nclass)
+    #         trainer = BatchTrainer(train_loader, test_loader, model)
+    #         trainer.minibatch_train()
+    #         print(f"time long: {time() - st}")
 
-    # for name, param in model.named_parameters():
-    #     print(name, param)
     pass
